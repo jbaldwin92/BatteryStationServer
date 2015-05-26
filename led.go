4ce9614b92b4553744122096321d94d1eeb3a72b
jbaldwin92 <jbaldwin92@gmail.com>
2015-05-25 20:27:14 -0700

    bug fix

--- led.go
+++ led.go
@@ -9,25 +9,26 @@ import (
   "github.com/jbaldwin92/bbb_io"   //bbb io functions
 )
 func main() {
-  led_init()
-  analog_init()
+  bbb_io.led_init()
+  bbb_io.analog_init()
   var value1, value2 float64
   var values1, values2 string
   for i:=0; i<5000; i++ {
-    led_off("1")
-    led_on("0")
+    bbb_io.led_off("1")
+    bbb_io.led_on("0")
     time.Sleep(time.Second*2)
-    led_on("1")
-    led_off("0")
+    bbb_io.led_on("1")
+    bbb_io.led_off("0")
     time.Sleep(time.Second*2)
-    value1 = analogReadN("P9_39",200) * 70.82  //36v conversion factor to volts
-    value2 = analogReadN("P9_37",200) * 1  //12v
+    value1 = bbb_io.analogReadN("P9_39",200) * 70.82  //36v conversion factor to volts
+    //TODO: Fix this conversion factor
+    value2 = bbb_io.analogReadN("P9_37",200) * 120  //12v
     values1 = strconv.FormatFloat(value1,'f',2,64)
     values2 = strconv.FormatFloat(value2,'f',2,64)
     fmt.Println(values1)
     fmt.Println(values2)
     fmt.Println("------")
   }
-  led_off("1")
-  led_off("0")
+  bbb_io.led_off("1")
+  bbb_io.led_off("0")
 }
