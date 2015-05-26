73ec1d3c0e2aedf72c31b89b15b4edc3f5e59469
jbaldwin92 <jbaldwin92@gmail.com>
2015-05-25 20:43:48 -0700

    Led to LED

--- led.go
+++ led.go
@@ -9,16 +9,16 @@ import (
   "github.com/jbaldwin92/bbb_io"   //bbb io functions
 )
 func main() {
-  bbb_io.Led_init()
+  bbb_io.LED_init()
   bbb_io.Analog_init()
   var value1, value2 float64
   var values1, values2 string
   for i:=0; i<5000; i++ {
-    bbb_io.Led_off("1")
-    bbb_io.Led_on("0")
+    bbb_io.LED_off("1")
+    bbb_io.LED_on("0")
     time.Sleep(time.Second*2)
-    bbb_io.Led_on("1")
-    bbb_io.Led_off("0")
+    bbb_io.LED_on("1")
+    bbb_io.LED_off("0")
     time.Sleep(time.Second*2)
     value1 = bbb_io.AnalogReadN("P9_39",200) * 70.82  //36v conversion factor to volts
     //TODO: Fix this conversion factor
@@ -29,6 +29,6 @@ func main() {
     fmt.Println(values2)
     fmt.Println("------")
   }
-  bbb_io.Led_off("1")
-  bbb_io.Led_off("0")
+  bbb_io.LED_off("1")
+  bbb_io.LED_off("0")
 }
