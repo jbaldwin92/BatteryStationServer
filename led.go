adeb36320694d15da93ef194044b131c9fbf35f3
Jeff <jbaldwin92@gmail.com>
2015-05-27 03:38:18 +0000

    Made some testing changes to led.go

--- led.go
+++ led.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "fmt"
-  "strconv"
   "time"
   "github.com/jbaldwin92/bbb_io"   //bbb io functions
 )
@@ -10,7 +9,6 @@ func main() {
   bbb_io.LED_init()
   bbb_io.Analog_init()
   var value1, value2 float64
-  var values1, values2 string
   for i:=0; i<5000; i++ {
     bbb_io.LED_off("1")
     bbb_io.LED_on("0")
@@ -20,13 +18,31 @@ func main() {
     time.Sleep(time.Second*2)
     value1 = bbb_io.AnalogReadN("P9_39",200) * 70.82  //36v conversion factor to volts
     //TODO: Fix this conversion factor
-    value2 = bbb_io.AnalogReadN("P9_37",200) * 120  //12v
-    values1 = strconv.FormatFloat(value1,'f',2,64)
-    values2 = strconv.FormatFloat(value2,'f',2,64)
-    fmt.Println(values1)
-    fmt.Println(values2)
+    value2 = bbb_io.AnalogReadN("P9_37",200) * 105.82  //12v
+    fmt.Printf("LiFePO4 Battery: %6.2fv %6.f %\n", value1, LiFePO4_SOC(value1/12) )
+    //fmt.Println(value1)
+    //fmt.Println(LiFePO4_SOC(value1/12) )
+    fmt.Printf("\nPb Battery:      %4.2fv\n",value2)
     fmt.Println("------")
   }
   bbb_io.LED_off("1")
   bbb_io.LED_off("0")
 }
+
+//input the voltage (per cell), the output is 0-100% state of charge
+func LiFePO4_SOC(v float64) float64 {
+  var SOC float64
+  if v>3.8 {
+    SOC=100.0
+  } else if v > 3.3 {
+    SOC = 100.0 - (3.8 - v) / (3.8 - 3.3) * (100.0-90.0)
+  } else if v > 3.2 {
+    SOC = 90.0 - (3.3-v) / (3.3-3.2) * (90-20)
+  } else if v > 2.0 {
+    SOC = 20 - (3.2-v) / (3.2 - 2.0) * (20-0)
+  } else {
+    SOC = 0.00
+  }
+  return SOC
+}
+
