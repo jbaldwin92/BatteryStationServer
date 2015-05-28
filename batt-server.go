524a26fc728486e8d82109a0ad62c854ae184735
Jeff <jbaldwin92@gmail.com>
2015-05-28 03:07:16 +0000

    some changes to led.go

--- batt-server.go
+++ batt-server.go
@@ -4,7 +4,10 @@ package main
 import (
 //  "fmt"
   "log"
+  "github.com/jbaldwin92/bbb_io"
   "net/http"
+  "strings"
+  "strconv"
   )
   
 func main() {
@@ -22,7 +25,11 @@ log.Fatal(http.ListenAndServe(":1721", nil))
 }
 
 func mainpage(w http.ResponseWriter, r *http.Request) {
-str1 := `<b>Batt Server</b>
+
+bbb_io.Analog_init()
+v := bbb_io.AnalogReadN("P9_39",100) * 71.13
+vs := strconv.FormatFloat(v,'f',2,64)
+str1 := `<h1>Batt Server</h1>
 <br>
 Eventually, you can choose which power source to use.
 <br>
@@ -30,7 +37,13 @@ Eventually, you can see the battery voltages and state of charge.
 <br>
 Eventually, you can see some power use plots, and see how much peak power has been saved.
 <br>
-Eventually, you can set the time when batteries are used, and the time when batteries are charged.`
+Eventually, you can set the time when batteries are used, and the time when batteries are charged.
+<br>
+Voltage = [[VOLTAGE]]`
+
+str1 = strings.Replace(str1,"[[VOLTAGE]]",vs,-1)
+
+
 
 w.Write([]byte(str1))
 }
