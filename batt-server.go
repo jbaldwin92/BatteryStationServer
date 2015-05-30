d05fa1f876526c2edc7da29a16eea44c7712e6b3
Jeff <jbaldwin92@gmail.com>
2015-05-30 04:23:01 +0000

    A better webpage

--- batt-server.go
+++ batt-server.go
@@ -2,48 +2,139 @@
 package main
 
 import (
-//  "fmt"
-  "log"
-  "github.com/jbaldwin92/bbb_io"
-  "net/http"
-  "strings"
-  "strconv"
-  )
-  
+	"fmt"
+	"github.com/jbaldwin92/bbb_io"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+        "time"
+)
+//------Global Variables
+        //analog pins
+        var AIN []string = []string{"P9_39","P9_40","P9_37","P9_38","P9_33","P9_36","P9_35"}
+        //calibration factors: AIN x K = Output
+        var K []float64 = []float64{70.82,105.82,1,1,1,1,1}
+        //GPIO pins for switching chargers and dischargers
+        var SW []string = []string{"P9_11"}
+
+
+//------Functions
 func main() {
 
-// Some Examples
-//http.Handle("/foo", fooHandler)
-//http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-//	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-//})
+        //Initialize the pins
+	bbb_io.Analog_init()
+        bbb_io.PinMode(SW[0],"OUTPUT")
+        //startup the datalogger (runs in parallel)
+        go v_logger()
 
-http.HandleFunc("/", mainpage)
+	// Some Examples
+	//http.Handle("/foo", fooHandler)
+	//http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	//})
 
+	http.HandleFunc("/", mainpage)
 
-log.Fatal(http.ListenAndServe(":1721", nil))
+	log.Fatal(http.ListenAndServe(":1721", nil))
 }
 
+//-----------------------------------------------------
 func mainpage(w http.ResponseWriter, r *http.Request) {
 
-bbb_io.Analog_init()
-v := bbb_io.AnalogReadN("P9_39",100) * 71.13
-vs := strconv.FormatFloat(v,'f',2,64)
-str1 := `<h1>Batt Server</h1>
-<br>
-Eventually, you can choose which power source to use.
-<br>
-Eventually, you can see the battery voltages and state of charge.
+	SOC1 := 0.00
+        SOC1s := ""
+        volt := make([]float64, 7)
+        volts:= make([]string, 7)
+        //read the voltages
+        for i:=0; i<7; i++ { 
+          volt[i] = bbb_io.AnalogReadN(AIN[i], 100)
+          volts[i] = strconv.FormatFloat(volt[i] * K[i], 'f', 2, 64)  //K[i] is the conversion factor
+        }
+        //check to see if any actions need to be taken
+//TODO: turn off discharge before turning on charger
+        if r.FormValue("SW0")=="on" { bbb_io.DigitalWrite(SW[0],"HIGH") }
+        if r.FormValue("SW0")=="off" { bbb_io.DigitalWrite(SW[0],"LOW") }
+//TODO: turn off charger before turning on discharger
+//TODO: discharge switch
+	//Now write out the page
+	str1 := `<h1>Batt Server</h1>
 <br>
 Eventually, you can see some power use plots, and see how much peak power has been saved.
 <br>
 Eventually, you can set the time when batteries are used, and the time when batteries are charged.
 <br>
-Voltage = [[VOLTAGE]]`
+<table>
+<tr>
+<th COLSPAN=3>Battery 1</th></tr>
+<tr>
+  <td>LiFePO4</td>
+  <td>12 cells</td>
+  <td>edit</td>
+</tr>
+<tr>
+  <td>Voltage</td>
+  <td>[[VOLTAGE1]]</td>
+  <td>calibrate</td>
+</tr>
+<tr>
+  <td>State of Charge</td>
+  <td>[[SOC1]]%</td>
+  <td></td>
+</tr>
+<tr>
+  <td>Charge</td>
+  <td>[[CHARGE1]]</td>
+  <td><a href="/?SW0=on">on</a> <a href="/?SW0=off">off</a></td>
+</tr>
+<tr>
+  <td>Discharge</td>
+  <td>[[DISCHARGE1]]</td>
+  <td><a href="/?SW1=on">on</a> <a href="/?SW1=off">off</a></td>
+</tr>
+</table>
+<a href="/">refresh</a>
+`
+        //calculate the SOC
+        SOC1 = SOC("LiFePO4",12,volt[0]*K[0])
+        SOC1s = strconv.FormatFloat(SOC1,'f',2,64)
+	//do the string substitutions
+	str1 = strings.Replace(str1, "[[VOLTAGE1]]", volts[0], -1)
+        str1 = strings.Replace(str1, "[[SOC1]]", SOC1s, -1)
+   	str1 = strings.Replace(str1, "[[CHARGE1]]", "Charger Off", -1)
+        str1 = strings.Replace(str1, "[[DISCHARGE1]]", "Supplying Power", -1)
 
-str1 = strings.Replace(str1,"[[VOLTAGE]]",vs,-1)
-
-
-
-w.Write([]byte(str1))
+	w.Write([]byte(str1))
 }
+
+//Calculate the State of Charge
+//Inputs: battery type, number of cells, and current voltage
+//Returns a number between 0 and 100. Will return -1 on error.
+func SOC(batType string, n int, v float64) float64 {
+  SOC := -1.0             //unless proven otherwise
+  v_cell := v/float64(n)  //voltage per cell
+  if batType=="LiFePO4" {
+    if v_cell>3.8 {
+      SOC=100.0
+    } else if v_cell > 3.3 {
+      SOC = 100 - (3.8 - v_cell) / (3.8-3.3) * (100.0-90.0)
+    } else if v_cell > 3.2 {
+      SOC = 90 - (3.3 - v_cell) / (3.3-3.2) * (90.0 - 20.0)
+    } else if v_cell > 2.0 {
+      SOC = 20 - (3.2 - v_cell) / (3.2-2.0) * (20.0 - 0.00)
+    } else {
+      SOC = 0.00
+    }
+  } else if batType=="Pb" {
+     SOC = 50.0
+  }
+  return SOC
+}
+
+//Data logger
+func v_logger() {
+  for {
+    fmt.Println(bbb_io.AnalogReadN(AIN[0],100)*K[0])
+    time.Sleep(time.Second*2)
+  }
+}  
--- led.go
+++ led.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "github.com/jbaldwin92/bbb_io"   //bbb io functions
+	"fmt"
+	"github.com/jbaldwin92/bbb_io" //bbb io functions
+	"time"
 )
+
 func main() {
-  bbb_io.LED_init()
-  bbb_io.Analog_init()
-  var value1, value2 float64
-  for i:=0; i<5000; i++ {
-    bbb_io.LED_off("1")
-    bbb_io.LED_on("0")
-    time.Sleep(time.Second*2)
-    bbb_io.LED_on("1")
-    bbb_io.LED_off("0")
-    time.Sleep(time.Second*2)
-    value1 = bbb_io.AnalogReadN("P9_39",200) * 70.82  //36v conversion factor to volts
-    //TODO: Fix this conversion factor
-    value2 = bbb_io.AnalogReadN("P9_37",200) * 105.82  //12v
-    fmt.Printf("LiFePO4 Battery: %6.2fv %6.f %\n", value1, LiFePO4_SOC(value1/12) )
-    //fmt.Println(value1)
-    //fmt.Println(LiFePO4_SOC(value1/12) )
-    fmt.Printf("\nPb Battery:      %4.2fv\n",value2)
-    fmt.Println("------")
-  }
-  bbb_io.LED_off("1")
-  bbb_io.LED_off("0")
+	bbb_io.LED_init()
+	bbb_io.Analog_init()
+	var value1, value2 float64
+	for i := 0; i < 5000; i++ {
+		bbb_io.LED_off("1")
+		bbb_io.LED_on("0")
+		time.Sleep(time.Second * 2)
+		bbb_io.LED_on("1")
+		bbb_io.LED_off("0")
+		time.Sleep(time.Second * 2)
+		value1 = bbb_io.AnalogReadN("P9_39", 200) * 70.82 //36v conversion factor to volts
+		//TODO: Fix this conversion factor
+		value2 = bbb_io.AnalogReadN("P9_37", 200) * 105.82 //12v
+		fmt.Printf("LiFePO4 Battery: %6.2fv %6.f %\n", value1, LiFePO4_SOC(value1/12))
+		//fmt.Println(value1)
+		//fmt.Println(LiFePO4_SOC(value1/12) )
+		fmt.Printf("\nPb Battery:      %4.2fv\n", value2)
+		fmt.Println("------")
+	}
+	bbb_io.LED_off("1")
+	bbb_io.LED_off("0")
 }
 
 //input the voltage (per cell), the output is 0-100% state of charge
 func LiFePO4_SOC(v float64) float64 {
-  var SOC float64
-  if v>3.8 {
-    SOC=100.0
-  } else if v > 3.3 {
-    SOC = 100.0 - (3.8 - v) / (3.8 - 3.3) * (100.0-90.0)
-  } else if v > 3.2 {
-    SOC = 90.0 - (3.3-v) / (3.3-3.2) * (90-20)
-  } else if v > 2.0 {
-    SOC = 20 - (3.2-v) / (3.2 - 2.0) * (20-0)
-  } else {
-    SOC = 0.00
-  }
-  return SOC
+	var SOC float64
+	if v > 3.8 {
+		SOC = 100.0
+	} else if v > 3.3 {
+		SOC = 100.0 - (3.8-v)/(3.8-3.3)*(100.0-90.0)
+	} else if v > 3.2 {
+		SOC = 90.0 - (3.3-v)/(3.3-3.2)*(90-20)
+	} else if v > 2.0 {
+		SOC = 20 - (3.2-v)/(3.2-2.0)*(20-0)
+	} else {
+		SOC = 0.00
+	}
+	return SOC
 }
-
