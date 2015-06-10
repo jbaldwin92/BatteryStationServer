969eb97bfef750fd91fc258abcb19942125141d4
Jeff <jbaldwin92@gmail.com>
2015-06-10 07:15:08 -0700

    Displays the voltage per cell

--- batt-server.go
+++ batt-server.go
@@ -15,6 +15,8 @@ import (
         var AIN []string = []string{"P9_39","P9_40","P9_37","P9_38","P9_33","P9_36","P9_35"}
         //calibration factors: AIN x K = Output
         var K []float64 = []float64{71.85,105.82,1,1,1,1,1}
+        //number of battery cells, for help with the SOC
+        var Cells []int64 = []int64{12,1,1,1,1,1,1}
         //GPIO pins for switching chargers and dischargers
         var SW []string = []string{"P9_11"}
 
@@ -49,10 +51,12 @@ func mainpage(w http.ResponseWriter, r *http.Request) {
         SOC1s := ""
         volt := make([]float64, 7)
         volts:= make([]string, 7)
+        volts_cell := make([]string,7)
         //read the voltages
         for i:=0; i<7; i++ { 
           volt[i] = bbb_io.AnalogReadN(AIN[i], 100)
           volts[i] = strconv.FormatFloat(volt[i] * K[i], 'f', 2, 64)  //K[i] is the conversion factor
+          volts_cell[i] = strconv.FormatFloat(volt[i] * K[i]/float64(Cells[i]), 'f', 2, 64)  //number of volts per cell
         }
         //check to see if any actions need to be taken
 //TODO: turn off discharge before turning on charger
@@ -82,13 +86,13 @@ Eventually, you can set the time when batteries are used, and the time when batt
 </tr>
 <tr>
   <td>Voltage</td>
-  <td>[[VOLTAGE1]]</td>
+  <td>[[VOLTAGE1]]v<br>[[VOLTAGE1_CELL]]v/cell</td>
   <td>calibrate</td>
 </tr>
 <tr>
   <td>State of Charge</td>
   <td>[[SOC1]]%</td>
-  <td>41.70V is about 100%</td>
+  <td>42.35V is about 100%</td>
 </tr>
 <tr>
   <td>Charge</td>
@@ -104,10 +108,11 @@ Eventually, you can set the time when batteries are used, and the time when batt
 <a href="/">refresh</a>
 `
         //calculate the SOC
-        SOC1 = SOC("LiFePO4",12,volt[0]*K[0])
+        SOC1 = SOC("LiFePO4",Cells[0],volt[0]*K[0])
         SOC1s = strconv.FormatFloat(SOC1,'f',2,64)
 	//do the string substitutions
 	str1 = strings.Replace(str1, "[[VOLTAGE1]]", volts[0], -1)
+        str1 = strings.Replace(str1, "[[VOLTAGE1_CELL]]", volts_cell[0], -1)
         str1 = strings.Replace(str1, "[[SOC1]]", SOC1s, -1)
    	str1 = strings.Replace(str1, "[[CHARGE1]]", "Charger Off", -1)
         str1 = strings.Replace(str1, "[[DISCHARGE1]]", "Supplying Power", -1)
@@ -119,17 +124,17 @@ Eventually, you can set the time when batteries are used, and the time when batt
 //Calculate the State of Charge
 //Inputs: battery type, number of cells, and current voltage
 //Returns a number between 0 and 100. Will return -1 on error.
-func SOC(batType string, n int, v float64) float64 {
+func SOC(batType string, n int64, v float64) float64 {
   SOC := -1.0             //unless proven otherwise
   v_cell := v/float64(n)  //voltage per cell
   if batType=="LiFePO4" {
     if v_cell>3.8 {
       SOC=100.01
-    } else if v_cell>3.475 {  //this is unique to my charger, which charges to 3.5v/cell
+    } else if v_cell>3.53 {  //this is unique to my charger, which charges to 3.5v/cell
       SOC=99.99
     } else if v_cell > 3.3 {
       //SOC = 100 - (3.8 - v_cell) / (3.8-3.3) * (100.0-90.0)  //this is the general value
-      SOC = 100 - (3.475 - v_cell) / (3.475-3.3) * (100 - 90.0)  //unique to my charger
+      SOC = 100 - (3.53 - v_cell) / (3.53-3.3) * (100 - 90.0)  //unique to my charger
     } else if v_cell > 3.2 {
       SOC = 90 - (3.3 - v_cell) / (3.3-3.2) * (90.0 - 20.0)
     } else if v_cell > 2.0 {
